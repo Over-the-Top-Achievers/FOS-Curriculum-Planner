b05f6c94d86883f5f39bee92c1db813f6d564e17
DxDenn <69644558+DxDenn@users.noreply.github.com>
2021-08-31 14:24:58 +0200

    test for degree(not complete)

--- src/app/admin-degree/admin-degree.component.spec.ts
+++ src/app/admin-degree/admin-degree.component.spec.ts
@@ -22,4 +22,27 @@ describe('AdminDegreeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should populate update form',()=>{
+    let course:any = {
+      //look u model view controller mvc
+      _id:"",
+      Degree_Name:"degreeTest",
+      Firm_Offer:"",
+      Waitlist:"",
+      Reject:"",
+    }
+    const spy = spyOn(component.updateDegreeForm,'setValue')
+    component.populateUpdate({
+      //look u model view controller mvc
+      _id:"",
+      Degree_Name:"degreeTest",
+      Firm_Offer:"",
+      Waitlist:"",
+      Reject:"",
+    })
+    delete course._id
+    expect(spy).toHaveBeenCalledWith(course)
+  
+  })
 });
