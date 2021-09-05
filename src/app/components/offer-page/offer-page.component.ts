68db4daf3a009aa3ca8e22086025b7d45d2a258b
Anushka Monema <anushka.monema@gmail.com>
2021-09-05 23:43:49 +0200

    More testing

--- src/app/components/offer-page/offer-page.component.ts
+++ src/app/components/offer-page/offer-page.component.ts
@@ -25,17 +25,16 @@ export class OfferPageComponent implements OnInit {
         this.settings = Object.assign({},this.settings);   
 
         // setInterval(()=> { this.updateQualifiedCoursese()}, 3 * 1000);
-        setInterval(()=> { this.addAPS()}, 1 * 1000);
-        setInterval(()=> { this.updateOffers()}, 1 * 1000);
-
-
+        
       }
     )
+
+    setInterval(()=> { this.addAPS()}, 1 * 1000);
+    setInterval(()=> { this.updateOffers()}, 1 * 1000);
 
     this.subjectService.getDegreeReq().subscribe(
       data => {
         this.degreeReqs = data as DegreeRequirement[];
-        this.qualifiedCoursesIII = new LocalDataSource();
         for(let i =0 ;i<this.degreeReqs.length;i++)
         {
           this.offerList.push({Degree_Name:this.degreeReqs[i].Degree_Name,Offer:"Reject"});
@@ -118,7 +117,9 @@ export class OfferPageComponent implements OnInit {
     if(offer===0) return "Reject";
     else if (offer===1) return "Waitlist";
     else if (offer===2) return "Firm Offer";
-    return "";
+    else{
+      return "";
+    }    
   }
   checkSubjectOffer(subjectValues:string[],subjectMark:Number):number //0 reject 1 wait 2 firm
   {
