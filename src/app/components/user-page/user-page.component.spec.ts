e0fa68c00de3f9ad315a0291970b4736343cc019
SushilRagoonath <48589447+SushilRagoonath@users.noreply.github.com>
2021-11-01 21:47:26 +0200

    remove test

--- src/app/components/user-page/user-page.component.spec.ts
+++ src/app/components/user-page/user-page.component.spec.ts
@@ -572,16 +572,6 @@ describe('UserPageComponent', () => {
     expect(spy).toHaveBeenCalled()
     // array should be empty
   })
-  it('should format string correctly',()=>{
-
-    component.missingCoReqInfo['test2'] = ['testco1','testco2']
-    component.missingPreReqInfo['test2'] = ['testpre1','testpre2']
-    let course:Course = {} as Course;
-    course.Course_Code = 'test2'
-    component.allYearClashes['test2'] = [course]
-    const result = component.formatRequirementInfo('test2')
-    expect(result).toEqual('Missing co-requisites: testco1,testco2 Missing pre-requisite: testpre1,testpre2 Possible clash:  testclash1')
-  })
 
   it('openCourseView should call service',()=>{
     const userServiceSpy= spyOn(component.userService, 'changeMessage').and.callThrough();
